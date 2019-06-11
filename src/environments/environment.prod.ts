79692a7abd97a4516031c7cc8d09ec4095dffc39
CarmeloRicarte <carmeloricarterocamora@gmail.com>
2019-06-11 20:29:36 +0200

    Actualización a Angular 8

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAjG-zogXzer5UmCz1LwsaabxZ_ZOS2v6g',
+    authDomain: 'firechat-crr.firebaseapp.com',
+    databaseURL: 'https://firechat-crr.firebaseio.com',
+    projectId: 'firechat-crr',
+    storageBucket: 'firechat-crr.appspot.com',
+    messagingSenderId: '995367352902',
+    appId: '1:995367352902:web:cd12acd13eaeb908'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAjG-zogXzer5UmCz1LwsaabxZ_ZOS2v6g',
+    authDomain: 'firechat-crr.firebaseapp.com',
+    databaseURL: 'https://firechat-crr.firebaseio.com',
+    projectId: 'firechat-crr',
+    storageBucket: 'firechat-crr.appspot.com',
+    messagingSenderId: '995367352902',
+    appId: '1:995367352902:web:cd12acd13eaeb908'
+  }
+};
